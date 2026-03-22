f814466f72219d0a01a84db348f248f0bcfefd10
Hrithikxcoder <gautamhrithik15@gmail.com>
2026-03-23 00:02:28 +0530

    Create program.cpp

---
+++ program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num % 2 == 0) {
+        cout << "The number is Even";
+    } else {
+        cout << "The number is Odd";
+    }
+
+    return 0;
+}
+
+
+"C:\Users\gauta\Pictures\Screenshots\Screenshot 2026-03-23 000030.png"
+
+
+
+  
